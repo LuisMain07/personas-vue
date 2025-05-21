ebbf7039647eadb86de8316f9fd2e6039ea4484d
LuisMain07 <bdmainguez00@gmail.com>
2025-05-21 01:15:05 -0500

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,31 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
+import Comunas from '@/views/Comunas.vue'
+import EditarComuna from '@/components/Comuna/EditarComuna.vue'
+import NewComuna from '@/components/Comuna/NewComuna.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/comunas',
+    name: 'Comunas',
+    component: Comunas
+  },
+    {
+    path: '/editar-comuna/:id',
+    name: 'EditarComuna',
+    component: EditarComuna
+  },
+  {
+    path: '/add-comuna/',
+    name: 'NewComuna',
+    component: NewComuna
   },
   {
     path: '/about',
